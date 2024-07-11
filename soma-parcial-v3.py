d820572c3b91d4b3e45af95b232765f08bc87d71
n4rus <lucasejs@live.com>
2024-07-11 02:54:02 -0300

    versao-2

--- soma-parcial-v3.py
+++ soma-parcial-v3.py
@@ -1,27 +1,28 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[7]:
+# In[36]:
 
 
 #cabeçalho obtido do repositorio "inset/intset"
 #codigo subsequente deve ser editado
 #def: a série "soma xn" é a sequencia de somas parciais de xn"" 
+print("Numero inteiro maior que 1")
 print("Digite o numero de parcelas xni 'tamanho da série':")
 
 #entrada do tamanho da série
 tamanho=input()
 
 
-# In[8]:
+# In[37]:
 
 
 #indice inicial
-print("Digite o indice inicial")
+print("Digite o indice inicial:")
 indice=input()
 
 
-# In[9]:
+# In[38]:
 
 
 lista=[]
@@ -40,17 +41,35 @@ while xni<=serie:
         partx=partx+1/xni
         xni=xni+1
         lista.append(partx)
+
+
+# In[40]:
+
+
 #adicionar input para print da lista "intervalo"
 #print(lista[900000:1000000])
-printl=lista[995000:1000000]
-print(printl)
+print("Digite o indice inicial a ser exibido (entre " + str(indice) + " e " + str(tamanho) + ").")
+indicei=input()
+print("Digite o indice final a ser exibido (entre " + str(indicei) + " e " + str(tamanho) + ").")
+indicef=input()
+
+indiceff=int(indicef)
+indiceii=int(indicei)
+printl=lista[indiceii:indiceff]
+print("\n" + str(printl))
 
 
-# In[10]:
+# In[26]:
 
 
 #escreve a lista em modo string no arquivo nomeado
 demo_file = open('lista.txt','a')
 demo_file.write(str(printl))
 demo_file.close()
+
+
+# In[ ]:
+
+
+
 
